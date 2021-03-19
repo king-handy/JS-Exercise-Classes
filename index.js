3c7a69b83da5fab6e1358c2eec28d26c457b745c
king-handy <a.king.life@pm.me>
2021-03-18 23:55:06 -0400

    Task1, Task2

--- index.js
+++ index.js
@@ -79,7 +79,28 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+
+    fill(gallons) {
+      this.tank += gallons;
+    }
+
+    drive(distance) {
+      if (this.tank > distance / this.milesPerGallon) {
+        this.odometer += distance;
+        this.tank -= distance / this.milesPerGallon;
+      }
+      else if (this.tank < distance / this.milesPerGallon) {
+        this.odometer += this.tank * this.milesPerGallon;
+        this.tank -= this.tank;
+        return `I ran out of fuel at ${this.odometer} miles!`
+      }
+    }
   }
   
   /*
